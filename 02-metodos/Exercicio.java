c65b8918a18ddebccfdd9e079679a33089baa6a7
Marcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2018-03-02 21:37:42 -0300

    exercicio

---
+++ 02-metodos/Exercicio.java
@@ -0,0 +1,38 @@
+class Exercicio {
+  public static void main(String[] args) {
+    // String charAt, length, equals
+    String s = "teste";
+    System.out.println(s.charAt(2));       // s
+    System.out.println(s.length());        // 5
+    System.out.println(s.equals("teste")); // true
+
+    // especificação
+    String t = "  palavra  ";
+    String l = Text.trimLeft(t);
+    System.out.println(l); // "palavra  "
+    System.out.println(l.equals("palavra  "));
+
+    String r = Text.trimRight(t);
+    System.out.println(r); // "  palavra"
+    System.out.println(r.equals("  palavra"));
+
+    String a = Text.trimAll(t);
+    System.out.println(a); // "palavra"
+    System.out.println(a.equals("palavra"));
+
+    String d = "  p  p  ";
+    System.out.println(Text.trimLeft(d).equals("p  p  "));
+    System.out.println(Text.trimRight(d).equals("  p  p"));
+    System.out.println(Text.trimAll(d).equals("p  p"));
+
+
+
+
+
+
+
+
+
+
+  }
+}
--- 02-metodos/Text.java
+++ 02-metodos/Text.java
@@ -1,14 +1,17 @@
 class Text {
-  static String padLeft(String s, int n, char c) {
-    int tam = s.length();
-    for (int i = 0; i < n - tam; i++) s = c + s;
+
+  static String pad(String s, int n, char c, char d) {
+    if (d == 'r') return padRight(s, n, c);
+    if (d == 'l') return padLeft(s, n, c);
     return s;
   }
 
+  static String padLeft(String s, int n, char c) {
+    return repeat(c, n - s.length()) + s;
+  }
+
   static String padRight(String s, int n, char c) {
-    int tam = s.length();
-    for (int i = 0; i < n - tam; i++) s = s + c;
-    return s;
+    return s + repeat(c, n - s.length());
   }
 
   static String repeat(char c, int n) {
