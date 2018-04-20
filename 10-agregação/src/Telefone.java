1e3ac223ac277856e5a9ec008d827c5402f4bfe5
Marcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2018-04-20 19:00:00 -0300

    eclipse agregacao

---
+++ 10-agregação/src/Telefone.java
@@ -0,0 +1,34 @@
+// classes abstratas são obrigadas a serem estendidas
+// não é possível dar "new" numa classe abstrata
+public abstract class Telefone {
+	
+	private String ddd;
+	private String numero;
+	
+	public Telefone(String ddd, String numero) {
+		super();
+		this.ddd = ddd;
+		this.numero = numero;
+	}
+
+	public Telefone(String numero) {
+		this(null, numero);
+	}
+	
+	public abstract String getTipo();
+
+	@Override
+	public String toString() {		
+		return getTipo() + " (" + ddd + ") " + numero;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
